07a1e625b696a46eaa2deb920848e12325ba7e6f
Yu <Yu.zhao1@rwth-aachen.de>
2023-03-05 14:43:19 +0100

    submission 3, correction of uptime format

--- src/format.cpp
+++ src/format.cpp
@@ -12,5 +12,11 @@ string Format::ElapsedTime(long seconds) {
   int hours = seconds / 3600;
   int minutes = (seconds - hours * 3600) / 60;
   int secs = seconds - hours * 3600 - minutes * 60;
-  return std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(secs);
+  string secsStr = std::to_string(secs);
+  secsStr.insert(0, 2 - secsStr.length(), '0');
+  string minStr = std::to_string(minutes);
+  minStr.insert(0, 2 - minStr.length(), '0');
+  string hoursStr = std::to_string(hours);
+  hoursStr.insert(0, 2 - hoursStr.length(), '0');
+  return hoursStr + ":" + minStr + ":" + secsStr;
 }
